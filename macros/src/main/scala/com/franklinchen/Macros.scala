ccde75f964ba5231ee64a960cf0aaa9a0401938d
Franklin Chen <franklinchen@franklinchen.com>
2014-06-12 23:19:15 -0400

    Simplify for 2.11.

--- macros/src/main/scala/com/franklinchen/Macros.scala
+++ macros/src/main/scala/com/franklinchen/Macros.scala
@@ -6,12 +6,11 @@ import scala.reflect.macros.blackbox.Context
 object Macros {
   def repeatMessage(n: Int): Unit = macro repeatMessageMacro
 
-  def repeatMessageMacro(c: Context)
-    (n: c.Expr[Int]): c.Expr[Unit] = {
+  def repeatMessageMacro(c: Context)(n: c.Tree) = {
     import c.universe._
 
     // Assume integer passed is constant
-    val Literal(Constant(num: Int)) = n.tree
+    val Literal(Constant(num: Int)) = n
 
     // Not only unroll, but also construct the strings at compile time
     val unrolledSeq = num to 1 by -1 map { i =>
@@ -20,6 +19,6 @@ object Macros {
       q"println($s)"
     }
 
-    c.Expr(q"{ ..$unrolledSeq }")
+    q"{ ..$unrolledSeq }"
   }
 }
