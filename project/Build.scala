98698c7cd0fc0dfc932a2c27e99aae7a9aeb3351
Franklin Chen <franklinchen@franklinchen.com>
2016-02-29 16:20:34 -0500

    Fix SBT build file.

--- project/Build.scala
+++ project/Build.scala
@@ -1,35 +1,32 @@
 import sbt._
 import Keys._
 
-object BuildSettings {
-  val buildSettings = Defaults.defaultSettings ++ Seq(
+object MyBuild extends Build {
+  override lazy val settings = super.settings ++ Seq(
     organization := "com.franklinchen",
     version := "1.0.0",
     scalaVersion := "2.11.7",
     scalacOptions += "-deprecation"
   )
-}
-
-object MyBuild extends Build {
-  import BuildSettings._
 
   lazy val root: Project = Project(
     "root",
-    file("."),
-    settings = buildSettings ++ Seq(
-      run <<= run in Compile in core)
-  ) aggregate(macros, core)
+    file(".")
+  ).settings(settings ++ Seq(
+      run <<= run in Compile in core
+    )
+  ).aggregate(macros, core)
 
   lazy val macros: Project = Project(
     "macros",
-    file("macros"),
-    settings = buildSettings ++ Seq(
-      libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-reflect" % _))
+    file("macros")
+  ).settings(settings ++ Seq(
+      libraryDependencies += "org.scala-lang" % "scala-reflect" % scalaVersion.value
+    )
   )
 
   lazy val core: Project = Project(
     "core",
-    file("core"),
-    settings = buildSettings
-  ) dependsOn(macros)
+    file("core")
+  ).dependsOn(macros)
 }
